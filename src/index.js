4ac9e83e04d78b073aef46cabb4624a0cabe3b5a
Yettafssaoui23 <yettafssaoui2@gmail.com>
2022-02-21 14:05:57 +0100

    adding redux to my app for the user

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+// This provider is a component that we want to wrap around the entire application
+import { Provider } from 'react-redux';
+
+import store from './redux/store';
 
 import './index.css';
 import App from './App';
 
+/**
+ * Provider : 
+ * this provider is a component that is the parent of everything inside of our application.
+ * And as the parent, 
+ * it allows us to get access to all of the things related to the store 
+ * that we're going to put all of the actual code we want to store on our redux state.
+ */
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
---
+++ src/redux/store.js
@@ -0,0 +1,16 @@
+/**
+ * we need to do is add middleware to our store
+ * whenever actions get fired or dispatched, 
+ * we can catch them and then display them.
+ *  the middleware, which is the peace in the middle between  actions firing and root reducer,
+ */
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+
+import rootReducer from './root-reducer';
+
+const middlewares = [logger];
+
+const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+export default store;
