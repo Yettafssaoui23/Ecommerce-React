51c2a76b2eae90e1447f8ae127513847c51c7951
Yettafssaoui23 <yettafssaoui2@gmail.com>
2022-02-24 00:35:01 +0100

    redux shop and directory state

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,12 +5,22 @@
  *  the middleware, which is the peace in the middle between  actions firing and root reducer,
  */
 import { createStore, applyMiddleware } from 'redux';
+/**
+ * it allows our browser to actually cache our store now depending 
+ * on certain configuration options that we're going to set
+ */
+import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
  
 import rootReducer from './root-reducer';
  
 const middlewares = [logger];
  
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+export const persistor = persistStore(store);
  
-export default store;
+export default { store, persistor };
+
+
+
