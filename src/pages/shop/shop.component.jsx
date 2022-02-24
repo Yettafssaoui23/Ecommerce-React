399d6b17b4cf4cb8c55f6eaedc46b293a767681b
Yettafssaoui23 <yettafssaoui2@gmail.com>
2022-02-24 22:47:12 +0100

    Changing the place of data

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,26 +1,17 @@
 import React from 'react';
 
-import { connect } from 'react-redux';
+import { Route } from 'react-router-dom';
 
-import { createStructuredSelector } from 'reselect';
+import CollectionsOverview from '../../components/collection-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview';
+const ShopPage = ({ match }) =>   {
+  return (
+  <div className='shop-page'>
+    <Route exact path={`${match.path}`} component={CollectionsOverview}/>
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+  </div>
+)};
 
-import { selectCollections } from '../../redux/shop/shop.selectors';
-
-const ShopPage = ({ collections }) =>   (
-    <div className='shop-page'>
-      {collections.map(({ id, ...otherCollectionProps }) => (
-        <CollectionPreview key={id} {...otherCollectionProps} />
-      ))}
-    </div>
-);
   
-
-
-
-const mapStateToProps = createStructuredSelector({
-  collections: selectCollections
-});
-
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
